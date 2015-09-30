0faf1ac0b7c1b03df1047242ad3629ed3ff42415
Matt Torgerson <Matt@Matts-MacBook.local>
2015-09-30 16:28:15 -0700

    Swift 2 - Electric Boogaloo

--- loyalty-demo/loyalty-demo/ViewController.swift
+++ loyalty-demo/loyalty-demo/ViewController.swift
@@ -11,9 +11,8 @@ import UIKit
 class ViewController: UIViewController, UIWebViewDelegate {
 
   @IBOutlet var kioskWebView: UIWebView!
-  
-  let uuid = "c3f31834-8e12-46d2-b159-62cbea2d3f18"
-  let token = "962f54fe236271a01f50a82b5e389bdde652e441"
+  let uuid = "6a605fd3-843a-4c2c-8776-e70c7bd23bef"
+  let token = "34efdd94193aa9f13fe982a422ee934c946bca08"
   
   override func viewDidLoad() {
     super.viewDidLoad()
